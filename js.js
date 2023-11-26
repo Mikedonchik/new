8dfb0529ef09da7c658912a211bde4f3b5774c74
Mikedonchik <mishamcpro008@gmail.com>
2023-11-26 19:52:58 +0200

    init

--- js.js
+++ js.js
@@ -8,6 +8,9 @@ $.ajax('https://my-json-server.typicode.com/Mikedonchik/new/products', {
             $(".all").html(
                 ` <div class ="card_two">
                 <img src="${e.photo_url}" alt="" class="photo_2">
+                <h1>${e.name}</h1>
+                <p >${e.price}</p>
+                <p>${e.description}</p>
                 </div >`
             )
         });
