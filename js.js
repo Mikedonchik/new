bf6d0b2c35534cf58e2f6b8405f1f245a2b481d0
Mikedonchik <mishamcpro008@gmail.com>
2023-12-03 19:39:05 +0200

    init

--- js.js
+++ js.js
@@ -11,6 +11,7 @@ $.ajax('https://my-json-server.typicode.com/Mikedonchik/new/products', {
                 <div class="disc">
                 <p >Price:${e.price}</p>
                 <p>Description:${e.description}</p>
+                <p class ="seller">Seller profile<p>
                 </div>
                 <button class="but">
                 <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" class="bi bi-plus" viewBox="0 0 16 16">
